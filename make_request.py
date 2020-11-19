c298f0ce118d9554badbed215d95f97827c9df7d
anusha <anusha@zipcodes-MacBook-Pro-6.local>
2020-11-19 15:15:44 -0500

    Data Acquisition

---
+++ make_request.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+offset = 1
+limit = 1000
+i = range(0,40)
+for k in i:
+    response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?limit=1000&offset='+str(offset), headers={'Token':'wQKbjZvMlUpgyMKjeeoFMUxdhbLHPvjz'})
+    j = json.loads(response.text)
+
+    with open('locations_'+str(k)+'.json', 'w') as json_file:
+         json.dump(j, json_file, indent=2)
+    offset += limit
