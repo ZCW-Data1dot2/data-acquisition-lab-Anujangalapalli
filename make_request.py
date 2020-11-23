64ccca57345cd07e8fc250d134a70ce55cfaa764
Anujangalapalli <60826485+Anujangalapalli@users.noreply.github.com>
2020-11-23 15:00:19 -0500

    Update make_request.py

--- make_request.py
+++ make_request.py
@@ -6,7 +6,7 @@ offset = 1
 limit = 1000
 i = range(0,40)
 for k in i:
-    response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?limit=1000&offset='+str(offset), headers={'Token':'wQKbjZvMlUpgyMKjeeoFMUxdhbLHPvjz'})
+    response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?limit=1000&offset='+str(offset), headers={'Token':'Key'})
     j = json.loads(response.text)
 
     with open('locations_'+str(k)+'.json', 'w') as json_file:
